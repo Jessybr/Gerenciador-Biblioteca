e5ef1303893a24d958d99de7bd03ee48d7c0f47c
Jessybr <jessicabuenoramos5@gmail.com>
2026-04-28 11:39:21 -0300

    :sparkles: feat: create User class

---
+++ src/main/java/com/Jessybr/Gerenciador_Biblioteca/domain/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.Jessybr.Gerenciador_Biblioteca.domain.model;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.Jessybr.Gerenciador_Biblioteca.domain.enums.RoleType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuario")
+@Entity
+public class Usuario implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate createdAt;
+    
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate updatedAt;
+    private boolean accountNonExpired = true;
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+}
