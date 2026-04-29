a33d33eca9f2fc467cc60f11202aeb4db28249d9
Jessybr <jessicabuenoramos5@gmail.com>
2026-04-29 15:29:49 -0300

    🩹 add @ActiveProfiles annotation to make mvn clean test pass

--- src/test/java/com/Jessybr/Gerenciador_Biblioteca/GerenciadorBibliotecaApplicationTests.java
+++ src/test/java/com/Jessybr/Gerenciador_Biblioteca/GerenciadorBibliotecaApplicationTests.java
@@ -2,8 +2,10 @@ package com.Jessybr.Gerenciador_Biblioteca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class GerenciadorBibliotecaApplicationTests {
 
 	@Test
