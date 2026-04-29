7f5ddf7defac5e77abb1719ee10e58f4650a338c
Jessybr <jessicabuenoramos5@gmail.com>
2026-04-29 15:28:44 -0300

    :sparkles: feat: add UserService

---
+++ src/main/java/com/Jessybr/Gerenciador_Biblioteca/service/UserService.java
@@ -0,0 +1,42 @@
+package com.Jessybr.Gerenciador_Biblioteca.service;
+
+import java.time.LocalDate;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.Jessybr.Gerenciador_Biblioteca.domain.enums.RoleType;
+import com.Jessybr.Gerenciador_Biblioteca.domain.model.Usuario;
+import com.Jessybr.Gerenciador_Biblioteca.dto.request.RegisterRequest;
+import com.Jessybr.Gerenciador_Biblioteca.dto.response.RegisterResponse;
+import com.Jessybr.Gerenciador_Biblioteca.exception.EmailAlreadyExistsException;
+import com.Jessybr.Gerenciador_Biblioteca.mapper.AuthMapper;
+import com.Jessybr.Gerenciador_Biblioteca.repository.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final AuthMapper mapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, AuthMapper mapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public RegisterResponse register(RegisterRequest request) {
+        if(!userRepository.findByUsername(request.username()).isEmpty()) {
+            throw new EmailAlreadyExistsException("Esse e-mail já está em uso.");
+        }
+
+        Usuario usuario = mapper.toEntity(request);
+        usuario.setPassword(passwordEncoder.encode(request.password()));
+        usuario.setRole(RoleType.ROLE_OPERADOR);
+        usuario.setCreatedAt(LocalDate.now());
+        usuario.setUpdatedAt(LocalDate.now());
+        userRepository.save(usuario);
+
+        return mapper.toResponse(usuario);
+    }
+}
