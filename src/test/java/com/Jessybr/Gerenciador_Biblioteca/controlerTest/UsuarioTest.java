30df037ef1b0f48a6b8f59c25af1408050a8c8ee
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-06 14:40:43 -0300

    :recycle: refactor: refactor test

--- src/test/java/com/Jessybr/Gerenciador_Biblioteca/controlerTest/UsuarioTest.java
+++ src/test/java/com/Jessybr/Gerenciador_Biblioteca/controlerTest/UsuarioTest.java
@@ -2,17 +2,19 @@ package com.Jessybr.Gerenciador_Biblioteca.controlerTest;
 
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDate;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import com.Jessybr.Gerenciador_Biblioteca.controller.UsuarioController;
 import com.Jessybr.Gerenciador_Biblioteca.dto.request.RegisterRequest;
@@ -20,28 +22,35 @@ import com.Jessybr.Gerenciador_Biblioteca.dto.response.RegisterResponse;
 import com.Jessybr.Gerenciador_Biblioteca.factory.UserRequestFactory;
 import com.Jessybr.Gerenciador_Biblioteca.factory.UserResponseFactory;
 import com.Jessybr.Gerenciador_Biblioteca.service.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(UsuarioController.class)
 public class UsuarioTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @MockitoBean
     private UsuarioService usuarioService;
 
     @Test
-    void shouldCreateNewUser_AndReturnStatus200() {
+    void shouldCreateNewUser_AndReturnStatus200() throws Exception{
         RegisterRequest request = UserRequestFactory.createRegisterRequest();
         RegisterResponse response = UserResponseFactory.createRegisterResponse();
 
         when(usuarioService.register(request)).thenReturn(response);
 
-        mockMvc.perform(get("/user/1"))
+        
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.username").value("novo_usuario"))
-            .andExpect(jsonPath("$.createAt").exists())
+            .andExpect(jsonPath("$.createdAt").exists())
             .andExpect(jsonPath("$.updatedAt").exists())
             .andExpect(jsonPath("$.accountNonExpired").value(true));
+
     }
 
 }
