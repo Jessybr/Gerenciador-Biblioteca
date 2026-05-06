b679b6957a0fe5c2865d88281f1835caa2cd695d
Jessybr <jessicabuenoramos5@gmail.com>
2026-05-06 14:39:33 -0300

    :sparkles: feat: create endpoint POST /register to test pass

--- src/main/java/com/Jessybr/Gerenciador_Biblioteca/controller/UsuarioController.java
+++ src/main/java/com/Jessybr/Gerenciador_Biblioteca/controller/UsuarioController.java
@@ -1,5 +1,28 @@
 package com.Jessybr.Gerenciador_Biblioteca.controller;
 
-public class UsuarioController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.Jessybr.Gerenciador_Biblioteca.dto.request.RegisterRequest;
+import com.Jessybr.Gerenciador_Biblioteca.dto.response.RegisterResponse;
+import com.Jessybr.Gerenciador_Biblioteca.service.UsuarioService;
+
+@RestController
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request) {
+        RegisterResponse response = usuarioService.register(request);
+
+        return ResponseEntity.ok(response);
+    }
+    
 }
